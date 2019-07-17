dac9606098e7646a39b37c80bfd80249c3fbf3b0
Alcino da Graça <alcinodagrsca@gmail.com>
2019-07-17 11:44:41 +0200

    Adicionei o CSS boilerplate e node stuff para server

---
+++ js/SimpleOperators.js
@@ -0,0 +1,10 @@
+let first = 12, 
+    second = 3;
+
+//let answer = first + second;  //Adição
+//let answer = first - second;  //Subtração
+//let answer = first * second;  //Multiplicaçãao
+let answer = first / second;  //Divisão
+//let answer = first % second;  //Resto
+
+console.log(answer);
---
+++ js/blackjack.js
@@ -0,0 +1,18 @@
+
+
+//Blackjack
+//by Alcino da Graça
+
+let carta1 = "Rei de Copas",
+    carta2 = "Rei de Ouros",
+    carta3 = "Rei de Espadas",
+    carta4 = "Dama de Paus",
+    carta5 = "Dama de Espadas";
+
+
+console.log("Você é tem: ");
+console.log(" " + carta1);
+console.log(" " + carta2);
+console.log(" " + carta3);
+console.log(" " + carta4);
+console.log(" " + carta5);
